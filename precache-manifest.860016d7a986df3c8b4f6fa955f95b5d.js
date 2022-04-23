29f0012ea48fce3e918737131d67ff6f4d98b5b9
Olena Grynevych <ezayak@gmail.com>
2022-04-23 10:04:02 +0200

    Updates

---
+++ precache-manifest.860016d7a986df3c8b4f6fa955f95b5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "245fc8d47b3bb3792600",
+    "url": "/static/css/main.6a1d14ca.chunk.css"
+  },
+  {
+    "revision": "245fc8d47b3bb3792600",
+    "url": "/static/js/main.245fc8d4.chunk.js"
+  },
+  {
+    "revision": "e7e0e04a5ea3842eb42e",
+    "url": "/static/js/1.e7e0e04a.chunk.js"
+  },
+  {
+    "revision": "30f7a241e25096ce33ad",
+    "url": "/static/js/2.30f7a241.chunk.js"
+  },
+  {
+    "revision": "6a4751f849122296e7c6",
+    "url": "/static/js/runtime~main.6a4751f8.js"
+  },
+  {
+    "revision": "02aa1cc474a72baf38953865cd14c8ab",
+    "url": "/index.html"
+  }
+];
