d7a079df420617b7364e20cde9d970caae9dea95
Olena Grynevych <ezayak@gmail.com>
2022-04-27 23:43:54 +0200

    version 0.2.1

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -32,6 +32,14 @@ const Main = () => {
         profile: 'referee'
     });
 
+    const compareData = (a, b) => {
+        if (a.time === b.time) {
+            return a.court - b.court;
+        } else {
+            return a.time > b.time ? 1 : -1;
+        }
+    };
+
     const loadMatches = (newDate) => {
         const q = query(collection(db, 'matches'), where('date', '==', new Timestamp(stringToTimeStamp(newDate), 0))); //, where('date', '==', new Timestamp(stringToTimeStamp(newDate), 0))
         return onSnapshot(q, (querySnapshot) => {
@@ -44,7 +52,8 @@ const Main = () => {
                     time: timeStampToTimeString(data.time),
                     date: timeStampToDate(data.date)
                 };
-            })]);
+                }).sort(compareData)
+            ]);
         });
     }
 
