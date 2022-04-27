66853d68fc0582be076ddcb4cb0fbfcdedcba2b3
Olena Grynevych <ezayak@gmail.com>
2022-04-27 22:57:16 +0200

    Updates

---
+++ precache-manifest.36a58727573e62a8368bf2ca92e53ea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e338da6f22e01b157678",
+    "url": "/dogeball-timer/static/css/main.0a66ef22.chunk.css"
+  },
+  {
+    "revision": "e338da6f22e01b157678",
+    "url": "/dogeball-timer/static/js/main.e338da6f.chunk.js"
+  },
+  {
+    "revision": "e7e0e04a5ea3842eb42e",
+    "url": "/dogeball-timer/static/js/1.e7e0e04a.chunk.js"
+  },
+  {
+    "revision": "37131c4f976ad0a6573d",
+    "url": "/dogeball-timer/static/js/2.37131c4f.chunk.js"
+  },
+  {
+    "revision": "3706ee5addfbfae272f1",
+    "url": "/dogeball-timer/static/js/runtime~main.3706ee5a.js"
+  },
+  {
+    "revision": "0610dc5d682feaa5fe0fcb0018fa07cb",
+    "url": "/dogeball-timer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e6a91b1ceeb657aa975fbcad888cc06a.js"
+  "/dogeball-timer/precache-manifest.36a58727573e62a8368bf2ca92e53ea6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/dogeball-timer/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
